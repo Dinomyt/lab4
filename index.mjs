1a4d307364a754c1e93e860f0287b603712594d8
Neng <nengithub@gmail.com>
2025-07-20 17:10:41 -0700

    +updated package

--- index.mjs
+++ index.mjs
@@ -72,6 +72,8 @@ app.get('/pluto', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('server started on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
+
